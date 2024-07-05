8288eedc00e6edff61916c21d5b0615497902b4a
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-07-05 18:09:01 +0530

    templatecutting

--- template cutting/Project/Project/settings.py
+++ template cutting/Project/Project/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
+TEMPLATES_DIRS=os.path.join(BASE_DIR,'vaishnavi')
+print(TEMPLATES_DIRS)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
