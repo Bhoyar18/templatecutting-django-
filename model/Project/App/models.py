2393fbaca75b7d17246d8bd115c43879e5544d89
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-07-17 18:50:55 +0530

    model

---
+++ model/Project/App/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    Name=models.CharField(max_length=70)
+    Email=models.EmailField()
+    Contact=models.IntegerField()
+    City=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.Name+' '+self.Email
+    # for changing the name .......
+    class Meta:
+        db_table = 'Student'
+        # django by default verbose name me 's' add krke deta hai 
+        # verbose_name = 'Student'
+        # we can give any name accordingly through this command 
+        verbose_name_plural = 'Student'
+        
+        # for changing name of students in decending order we use (-) sign 
+        ordering = ['-Name']
+
+        # for changing in ascending order we will remove the  (-)sign
+        # ordering = ['Name']
+
+# ==================================================================================
